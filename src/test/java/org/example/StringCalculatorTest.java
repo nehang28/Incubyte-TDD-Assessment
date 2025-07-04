6398f3964d4938e0be4f631e855a5679d290622b
nehang28 <nehangpatel0028@gmail.com>
2025-07-04 19:06:54 +0530

    Added basic test of empty string

--- src/test/java/org/example/StringCalculatorTest.java
+++ src/test/java/org/example/StringCalculatorTest.java
@@ -1,4 +1,11 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class StringCalculatorTest {
+    @Test
+    public void test() {
+        Assertions.assertEquals(0, StringCalculator.add(""));
+    }
 }
