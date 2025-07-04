8852bc38cdc2e18614158d481cdb31bc449a728f
nehang28 <nehangpatel0028@gmail.com>
2025-07-04 19:24:23 +0530

    Added test for single number string return that number

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class StringCalculator {
     public static int add(String input) {
+        if(input.isEmpty()) {
+            return 0;
+        }
+        String[] numbers = input.split(",");
+        if(numbers.length == 1) {
+            return Integer.parseInt(numbers[0]);
+        }
         return 0;
     }
 }
--- src/test/java/org/example/StringCalculatorTest.java
+++ src/test/java/org/example/StringCalculatorTest.java
@@ -5,7 +5,13 @@ import org.junit.jupiter.api.Test;
 
 public class StringCalculatorTest {
     @Test
-    public void test() {
+    public void emptyStringReturnsZero() {
         Assertions.assertEquals(0, StringCalculator.add(""));
+    }
+
+    @Test
+    public void singleNumberStringReturnsThatNumber() {
+        Assertions.assertEquals(8, StringCalculator.add("8"));
+        Assertions.assertEquals(15, StringCalculator.add("15"));
     }
 }
